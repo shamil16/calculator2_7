ca24e7a140a744fde9aef1338120b41207aaedb4
shamil16 <chernovshamil6@gmail.com>
2022-08-31 14:00:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/calculator2_7/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.calculator2_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onNumberClick(View view) {
+    }
+
+    public void onPut2Activity(View view) {
+    }
+
+    public void onOperationClick(View view) {
+    }
+}
